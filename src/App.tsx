7d808c0c368a202a20da43bf4199d7d4effe72c7
Hasibur Rahman <hasiburdev@gmail.com>
2022-09-28 14:48:44 +0600

    Setup vite with typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Login from './pages/login';
+import Register from './pages/register';
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1 className="text-white bg-blue-400 font-xl">Login</h1>;
+}
+
+export default Login;
